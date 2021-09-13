f3cc87bc4255cf7715cab269fe69a8453814bac0
Susanna Hammarberg <sanna.ham4@gmail.com>
2021-09-13 09:37:49 +0200

    plot the errors

--- plotscript_2drecons.py
+++ plotscript_2drecons.py
@@ -16,10 +16,13 @@ from plotPtypyResults import plot2drecons
 import matplotlib
 matplotlib.use( 'Qt5agg' )
 
+#%%
 
 date_saved = 20210910
-projection = 31
-itstr = 'iter0' 
+projection = 31 +5 
+itstr = 'iter100' 
+
+save = False
 
 openpath = r'C:\Users\Sanna\Documents\Simulations\save_simulation\recons\%s_projection%i'%(date_saved,projection)
 
@@ -31,9 +34,12 @@ obj = np.load(openpath + '\\'  + 'object_%s.npy'%itstr, allow_pickle=True)
 x = np.squeeze(np.load(openpath + '\\' + 'x_%s.npy'%itstr)) 
 y = np.squeeze(np.load(openpath + '\\' + 'y_%s.npy'%itstr)) 
 z = np.squeeze(np.load(openpath + '\\' + 'z_%s.npy'%itstr)) 
-errors =
-ferrors = 
+#errors = np.load(openpath+ '\\errors.npy')
+ferrors = np.load(openpath+ '\\ferrors.npy')
 
+plt.figure()
+plt.plot(errors,'blue')
+plt.plot(abs(ferrors),'red')
 
 #TODO which coordinate, x,y,z?
 psize = x[1,0,0]-x[0,0,0]
@@ -41,6 +47,12 @@ psize = x[1,0,0]-x[0,0,0]
 extent = 1e6 * np.array([0,(obj.shape[1]-1)*psize, 0, (obj.shape[0]-1)*psize])
 
 
-plot2drecons(obj.T, probe, extent, savepath, save=False)
+if save == True:
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+        print('new folder in this savepath was created')
 
 
+plot2drecons(obj.T, probe, extent, savepath, save)
+
+    
